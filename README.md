b9cb65ea2fcfb4f5a49a22e254b2f530954bce87
Maycon <mayconjf@gmail.com>
2020-10-31 10:54:00 -0300

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Flutter Login Animation
+
+Projeto em flutter para testar os aprendizados em animações de interfaces em uma tela simples de login e tela de boas vindas ;)
+
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login_animation/screens/login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
